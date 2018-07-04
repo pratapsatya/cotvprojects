ef2129aaaa70aaba742d8eaa1aa5149d295c3040
@pratapsatya <laharipratap7@gmail.com>
2018-07-04 15:26:23 +0530

    fourth demo

---
+++ fourth.java
@@ -0,0 +1,7 @@
+public class fourth
+{
+  public static void main(String[] args)
+	{
+		System.out.println("fourth java program");
+	}
+}
--- second.java
+++ second.java
@@ -1,4 +1,4 @@
-public class second()
+public class second
 {
 
    public static void main(String[] args)
